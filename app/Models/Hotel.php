7594ee74b95c4fcdd6e62d4e8601dfabed01bff7
Sofia <sofiaamv2003@gmail.com>
2025-04-09 20:53:38 -0500

    Modelo Hoteles

---
+++ app/Models/Hotel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Hotel extends Model
+{
+    use HasFactory;
+    protected $table = 'hotels';
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+}
